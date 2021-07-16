48980221dc6906599dbafd61a4c275f7017dd8b0
tesseract24 <kakha.kakhadze.1@btu.edu.ge>
2021-07-16 16:42:39 +0400

    b1

---
+++ 9_1/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main(void)
+{
+    
+    
+    
+
+    
+    
+
+    printf("Range of signed int is [%d, %d].\n", INT_MIN, INT_MAX);
+    printf("Range of unsigned int is [%u, %u].\n", 0, UINT_MAX);
+
+    printf("Range of signed long int is [%ld, %ld].\n", LONG_MIN, LONG_MAX);
+    printf("Range of unsigned long int is [%lu, %lu].\n", 0, ULONG_MAX);
+
+    return 0;
+}
+
+
+
